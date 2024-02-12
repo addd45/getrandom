1f29ab05fd53f837208fef92f528d6e9f04f6fa2
addd45 <33461111+addd45@users.noreply.github.com>
2024-02-12 13:53:59 -0600

    Update error_impls.rs

--- src/error_impls.rs
+++ src/error_impls.rs
@@ -1,8 +1,28 @@
+// Copyright 2018 Developers of the Rand project.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+#[cfg(not(feature = "mesalock_sgx"))]
 extern crate std;
 
-use crate::Error;
+use crate::{error::UNKNOWN_IO_ERROR, Error};
 use core::convert::From;
+use core::num::NonZeroU32;
 use std::io;
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        if let Some(errno) = err.raw_os_error() {
+            if let Some(code) = NonZeroU32::new(errno as u32) {
+                return Error::from(code);
+            }
+        }
+        UNKNOWN_IO_ERROR
+    }
+}
 
 impl From<Error> for io::Error {
     fn from(err: Error) -> Self {
